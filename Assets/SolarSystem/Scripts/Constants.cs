e09ff675027a196bf02e3683100d8a7650286fce
Felipe Andrés Alvarez Avaria <alu0100969535@ull.edu.es>
2023-06-15 20:58:03 +0100

    Initial commit

---
+++ Assets/SolarSystem/Scripts/Constants.cs
@@ -0,0 +1,11 @@
+﻿namespace SolarSystem {
+	public static class Constants {
+
+		public const float YEAR_DURATION_IN_SECONDS = 300.0f;
+		public const float DAY_DURATION_IN_SECONDS = YEAR_DURATION_IN_SECONDS / DAYS_IN_YEAR;
+		
+		
+		public const float DAYS_IN_YEAR = 365.25f;
+
+	}
+}
